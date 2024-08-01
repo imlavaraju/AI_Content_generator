ebceaf759f3aee55ef333c37c34519b940d38250
LAVARAJU GORLI <141550385+imlavaraju@users.noreply.github.com>
2024-08-02 00:14:41 +0530

    Update chatGPT.js

--- frontend/src/apis/chatGPT/chatGPT.js
+++ frontend/src/apis/chatGPT/chatGPT.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const generateContentAPI = async (userPrompt) => {
   const response = await axios.post(
-    "https://ai-content-generator-lsi9-kyyka1wgl-lavarajus-projects.vercel.app/api/v1/openai/generate-content",
+    "https://ai-content-generator-lsi9-git-main-lavarajus-projects.vercel.app/api/v1/openai/generate-content",
     {
       prompt: userPrompt,
     },
