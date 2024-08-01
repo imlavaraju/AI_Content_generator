c684f9985c259150c8f0624ad8e29e0c50659a3e
LAVARAJU GORLI <141550385+imlavaraju@users.noreply.github.com>
2024-08-01 23:32:52 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -101,7 +101,7 @@ const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
-//app.use(cors(corsOptions));
+//app.use(cors());
 //----Routes-----
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/openai", openAIRouter);
